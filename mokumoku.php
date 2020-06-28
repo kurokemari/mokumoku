27eef37c130df223003fc29028d540fba3bbb238
くろけまり <sime0113@gmail.com>
2020-06-28 16:34:47 +0900

    JS作った

--- mokumoku.php
+++ mokumoku.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>mokumoku</title>
 </head>
 <body>
     
+<canvas id="myChart" width="400" height="400"></canvas>
+
+
 </body>
 </html>
