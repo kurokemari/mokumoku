387382a380868b814a82ba8f7eac2ea29f13b65f
くろけまり <sime0113@gmail.com>
2020-07-01 22:50:39 +0900

    変更

--- mokumoku.php
+++ mokumoku.php
@@ -6,9 +6,19 @@
     <title>mokumoku</title>
 </head>
 <body>
-    
-<canvas id="myChart" width="400" height="400"></canvas>
 
+<h1>グラフ</h1>
+<!-- <canvas id="myChart" width="400" height="400"></canvas>
+　　　　　　　　<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
+    <script>
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: タイプ,
+      data: データ,
+
+    });
+    </script> -->
 
 </body>
+<script src="Chart.js"></script>
 </html>
